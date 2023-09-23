c4c2abdd47c9f97b785f15f5a8c51587d5537602
ikaz1 <bederizaki@gmail.com>
2023-09-23 15:13:16 +0800

    Refactor localStorage retrieval

--- src/utils/retrieveStore.js
+++ src/utils/retrieveStore.js
@@ -4,13 +4,12 @@ import Todo from '../todo';
 import projects from '../store';
 
 function retrieveStore() {
-    const storage = localStorage.getItem('projects');
-
-    if (storage === null) {
+    if (localStorage.getItem('projects') === null) {
         // Create intial page load welcome project
         const welcome = new Project(0, 'Welcome! 🎉', '', '#3b96eb');
 
         projects.user.push(welcome);
+
         welcome.todos.push(
             new Todo(0, 0, 'Get started with personal projects 🛠️'),
             new Todo(1, 0, 'Create, edit, and mark tasks as complete ✔️'),
@@ -19,6 +18,7 @@ function retrieveStore() {
             new Todo(4, 0, 'Organize tasks by sorting and filtering ⬆️⬇️'),
             new Todo(5, 0, 'Customize with themes and colors 🎨')
         );
+
         welcome.todos.forEach((todo, index) => {
             todo.addedDate = add(todo.addedDate, { seconds: index });
         });
@@ -30,48 +30,31 @@ function retrieveStore() {
         return;
     }
 
-    const data = JSON.parse(storage, (key, value) =>
-        key.endsWith('Date') ? parseISO(value) : value
+    // Get projects and todos from localStorage and reinitialize with constructors
+    const storage = JSON.parse(
+        localStorage.getItem('projects'),
+        (key, value) => (key.endsWith('Date') ? parseISO(value) : value)
     );
 
     projects.default = [];
     projects.user = [];
 
-    data.default.forEach((project) => {
-        projects.default.push({ ...project, todos: [] });
-    });
-
-    data.user.forEach((project) => {
-        const newProject = new Project(
-            project.id,
-            project.name,
-            project.description,
-            project.color
-        );
-
-        newProject.filter = project.filter;
-        newProject.sortOrder = project.sortOrder;
-        newProject.sortBy = project.sortBy;
-
-        project.todos.forEach((todo) => {
-            const newTodo = new Todo(
-                todo.id,
-                todo.projectId,
-                todo.task,
-                todo.dueDate,
-                todo.starred
-            );
-
-            newTodo.addedDate = todo.addedDate;
-            newTodo.completed = todo.completed;
-
-            newProject.todos.push(newTodo);
+    storage.default.forEach((project) => {
+        projects.default.push({
+            ...project,
+            todos: project.todos.map((todo) => Object.assign(new Todo(), todo))
         });
-
-        projects.user.push(newProject);
     });
 
-    projects.update();
+    storage.user.forEach((project) => {
+        projects.user.push(
+            Object.assign(new Project(), project, {
+                todos: project.todos.map((todo) =>
+                    Object.assign(new Todo(), todo)
+                )
+            })
+        );
+    });
 }
 
 export default retrieveStore;
