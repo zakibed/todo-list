7fa5110e9965f337e63cdd361c0c4de7007cc1c1
ikaz1 <bederizaki@gmail.com>
2023-10-09 18:04:27 +0800

    Configure babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,23 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }]
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }]
+                        ]
+                    }
+                }
+            }
+        ]
     }
 };
