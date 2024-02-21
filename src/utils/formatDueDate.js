161bf9c270fe3c6b336879621e6bce314a4e17ac
ikaz1 <bederizaki@gmail.com>
2024-02-21 19:05:37 +0800

    Refactor date formatting

--- src/utils/formatDueDate.js
+++ src/utils/formatDueDate.js
@@ -1,18 +1,13 @@
-import format from 'date-fns/format';
-import addDays from 'date-fns/addDays';
-import getYear from 'date-fns/getYear';
-import differenceInYears from 'date-fns/differenceInYears';
-import isValid from 'date-fns/isValid';
-import isYesterday from 'date-fns/isYesterday';
-import isToday from 'date-fns/isToday';
-import isTomorrow from 'date-fns/isTomorrow';
-import isSunday from 'date-fns/isSunday';
-import isMonday from 'date-fns/isMonday';
-import isTuesday from 'date-fns/isTuesday';
-import isWednesday from 'date-fns/isWednesday';
-import isThursday from 'date-fns/isThursday';
-import isFriday from 'date-fns/isFriday';
-import isSaturday from 'date-fns/isSaturday';
+import {
+    format,
+    addDays,
+    getYear,
+    differenceInYears,
+    isValid,
+    isYesterday,
+    isToday,
+    isTomorrow
+} from 'date-fns';
 
 function formatDueDate(dueDate) {
     if (!isValid(dueDate)) return '';
@@ -32,17 +27,11 @@ function formatDueDate(dueDate) {
     }
 
     if (isYesterday(dueDate)) return 'Yesterday';
-    if (isToday(dueDate)) return 'Today';
     if (isTomorrow(dueDate)) return 'Tomorrow';
+    if (isToday(dueDate)) return 'Today';
 
     if (dueDate <= addDays(currentDate, 7) && dueDate >= currentDate) {
-        if (isSunday(dueDate)) return 'Sunday';
-        if (isMonday(dueDate)) return 'Monday';
-        if (isTuesday(dueDate)) return 'Tuesday';
-        if (isWednesday(dueDate)) return 'Wednesday';
-        if (isThursday(dueDate)) return 'Thursday';
-        if (isFriday(dueDate)) return 'Friday';
-        if (isSaturday(dueDate)) return 'Saturday';
+        return format(dueDate, 'EEEE');
     }
 
     return format(dueDate, 'd MMM');
